fd73ceefc85630b89e2c046a71dd8d36360a58a5
NikolayKochnev <163389484+NikolayKochnev@users.noreply.github.com>
2024-03-18 16:34:13 +0700

    Add files via upload

---
+++ homework3.1.py
@@ -0,0 +1,9 @@
+my_string = input('Введите координаты цели: ')
+print(len(my_string))
+
+my_string = 'Urban students'
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0:1])
+print(my_string[-1])
